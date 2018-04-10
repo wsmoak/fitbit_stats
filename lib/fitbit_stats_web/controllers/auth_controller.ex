0e42e55c7889d2691a1f5acf0b76fae19ef94e20
Wendy Smoak <wsmoak@gmail.com>
2018-04-09 21:55:20 -0400

    Use the token to retrieve the profile data and store it

--- lib/fitbit_stats_web/controllers/auth_controller.ex
+++ lib/fitbit_stats_web/controllers/auth_controller.ex
@@ -1,13 +1,35 @@
 defmodule FitbitStatsWeb.AuthController do
   use FitbitStatsWeb, :controller
 
+  alias FitbitStats.Accounts.User
+  alias FitbitStats.Repo
+
   def index(conn, _params) do
-    redirect conn, external: Fitbit.authorize_url!(scope: "settings sleep")
+    redirect conn, external: Fitbit.authorize_url!(scope: "activity nutrition profile")
   end
 
   def callback(conn, %{"code" => code}) do
     token = Fitbit.get_token!(code: code)
+    IO.puts "THE TOKEN IS..."
     IO.inspect token
-    redirect conn, to: "/"
+
+    data = OAuth2.Client.get!(token, "/1/user/-/profile.json").body
+    IO.puts "THE DATA IS..."
+    IO.inspect data
+
+    user_id = data["user"]["encodedId"]
+    user_name = data["user"]["displayName"]
+
+    changeset = User.changeset(%User{},
+      %{user_id: user_id,
+        access_token: token.token.access_token,
+        refresh_token: token.token.refresh_token,
+        name: user_name
+      })
+    Repo.insert!(changeset)
+
+    conn
+    |> put_flash(:info, "Hello #{user_name}!")
+    |> redirect(to: "/")
   end
 end
